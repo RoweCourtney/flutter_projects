b6967659f954c9614591cd8cf3d5c78500bfd0bc
Courtney <courtney.c.rowe1@gmail.com>
2023-09-18 16:20:56 -0700

    add upgraded categores w/ images & some button functionality / page navigation

--- meals_app/lib/screens/meals.dart
+++ meals_app/lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -13,45 +14,34 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    Widget content = ListView.builder(
-      itemBuilder: (ctx, index) => Text(
-        meals[index].title,
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            "Uh oh ... nothing's here!",
+            style: Theme.of(context)
+                .textTheme
+                .headlineLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Text(
+            "Try selecting another category!",
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
       ),
     );
 
-    if (meals.isEmpty) {
-      content = Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              "Uh oh ... nothing's here!",
-              style: Theme.of(context)
-                  .textTheme
-                  .headlineLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Text(
-              "Try selecting another category!",
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-            ),
-          ],
-        ),
-      );
-    }
-
     if (meals.isNotEmpty) {
       content = ListView.builder(
-        itemCount: meals.length,
-        itemBuilder: (ctx, index) => Text(
-          meals[index].title,
-        ),
-      );
+          itemCount: meals.length,
+          itemBuilder: (ctx, index) => MealItem(meal: meals[index]));
     }
 
     return Scaffold(
